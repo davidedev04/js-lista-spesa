bf945a6abd6bcfbe5f146468fcc0dd43a2be280c
Davide Prencipe <davidedev04@gmail.com>
2023-11-24 17:26:16 +0100

    fine esercizio

--- spesa.js
+++ spesa.js
@@ -1,5 +1,5 @@
 
-const listaUl = document.querySelector(".lista_spesa")
+let listaUl = document.getElementById("lista_spesa")
 
 // creiamo un array con gli ingredienti dentro
 const spesa = ["pomodori", "patate", "cipolle", "sottilette", "tonno", "patatine", "merendine"];
@@ -9,12 +9,25 @@ let i = 0;
 
 // facciamo un ciclo per creare la lista
 
-
+let ingrediente;
 
 do {
+
     let ingrediente = spesa[i];
+
     console.log(ingrediente);
+
     i++;
-    
+
+
+    // creiamo un li 
+    const ingredienteLi = document.createElement("li");
+
+    // aggiungiamoli il contenuto
+    ingredienteLi.append(ingrediente);
+
+    // aggiungiamo l'li all'ul
+    listaUl.append(ingredienteLi);
+
 
 } while (i < spesa.length);
